516aad45156d1541f0a83643b89482a50838cbc2
justno1 <justno1@hotmail.com.tw>
2015-05-06 14:37:09 +0800

    test

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@
  //初始化Parse();
   Parse.initialize("lb68hfMRYgqhmuro8Ook5xux5mORCjF59w6Exfek","PAQaaFUhvx52GXTHFsZ4MeLF2aaQNrJT1xx6je2S");
 
-      console.log("test");
-
  var handler = {
 
    logInViewFunc: function(redirect){
@@ -12,28 +10,23 @@
   //   綁定註冊表單的註冊檢查事件(); // 送出還要再檢查一次，這裡會用Parse.User.signUp和相關函數
      document.getElementById('form-signup').addEventListener('submit',function(j){
       j.preventDefault();
-
       var user = new Parse.User();
       user.set("username",document.getElementById('form-signup-student-id').value);
       user.set("password",document.getElementById('form-signup-password').value);
       user.set("email",document.getElementById('form-signup-email').value);
-
-      console.log("test");
 
       user.signUp(null,{
         success: function(user){
       //    postAction();
         },
         error: function(user,error){
-          document.getElementById('form-signUP-message').innerHTML = error.message + '[' + error.code + ']';
+          document.getElementById('form-signin-message').innerHTML = error.message + '[' + error.code + ']';
         }
       });
     },false);
       }
-      document.getElementById('form-signUp-message').innerHTML = "TEST";
+      document.getElementById('form-signin-message').innerHTML = "TEST";
     };
-
-
 
   this.Router = new router();
   Parse.history.start();
